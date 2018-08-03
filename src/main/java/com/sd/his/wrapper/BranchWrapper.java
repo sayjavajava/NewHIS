62d000d85088da85fe62dd363160ec9ecfec8329
Qari Jamal <jamal@solutiondots.net>
2018-08-03 17:57:56 +0500

    branch Wrapper

--- src/main/java/com/sd/his/wrapper/BranchWrapper.java
+++ src/main/java/com/sd/his/wrapper/BranchWrapper.java
@@ -1,6 +1,11 @@
 package com.sd.his.wrapper;
 
 import com.sd.his.model.Branch;
+import com.sd.his.model.Doctor;
+import com.sd.his.model.Room;
+import com.sd.his.model.User;
+
+import java.util.List;
 
 /*
  * @author    : Qari Muhammad Jamal
@@ -26,15 +31,40 @@ import com.sd.his.model.Branch;
  */
 public class BranchWrapper {
 
-    long id;
-    String name;
-    String billingBranchName;
-    String billingTaxId;
-    int noOfRooms;
-    boolean primaryBranch;
-    boolean primaryDr;
-    boolean billingBranch;
+    private String billingBranchName;
+    private String billingTaxId;
+    private int noOfRooms;
+    private boolean primaryBranch;
+    private  boolean primaryDr;
     private boolean checkedBranch;
+    private  long id;
+    private String branchName;
+    private String name;
+    private  String officeHoursStart;
+    private  String officeHoursEnd;
+    private  Integer noOfExamRooms;
+    private  String state;
+    private  String city;
+    private  String primaryDoctor;
+    private  Integer zipCode;
+    private  String country;
+    private  String officePhone;
+    private  String fax;
+    private  String formattedAddress;
+    private  String billingName;
+    private  String billingBranch;
+    private  String billingTaxID;
+    private Boolean showBranchOnline;
+    private  Boolean allowOnlineSchedulingInBranch;
+    private  Long rooms;
+    private  String username;
+    private   List<Room> examRooms;
+    private  String address;
+    private  User user;
+    private  Room roomList;
+    private  Doctor doctor;
+    private  String firstName;
+    private  String lastName;
 
     public BranchWrapper() {
     }
@@ -52,7 +82,6 @@ public class BranchWrapper {
     public BranchWrapper(Branch branch) {
         this.id = branch.getId();
         this.name = branch.getName();
-//        this.billingBranch = branch.getBillingBranchName();
     }
 
     public long getId() {
@@ -111,19 +140,219 @@ public class BranchWrapper {
         this.primaryDr = primaryDr;
     }
 
-    public boolean isBillingBranch() {
-        return billingBranch;
-    }
-
-    public void setBillingBranch(boolean billingBranch) {
-        this.billingBranch = billingBranch;
-    }
-
     public boolean isCheckedBranch() {
         return checkedBranch;
     }
 
     public void setCheckedBranch(boolean checkedBranch) {
         this.checkedBranch = checkedBranch;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getOfficeHoursStart() {
+        return officeHoursStart;
+    }
+
+    public void setOfficeHoursStart(String officeHoursStart) {
+        this.officeHoursStart = officeHoursStart;
+    }
+
+    public String getOfficeHoursEnd() {
+        return officeHoursEnd;
+    }
+
+    public void setOfficeHoursEnd(String officeHoursEnd) {
+        this.officeHoursEnd = officeHoursEnd;
+    }
+
+    public Integer getNoOfExamRooms() {
+        return noOfExamRooms;
+    }
+
+    public void setNoOfExamRooms(Integer noOfExamRooms) {
+        this.noOfExamRooms = noOfExamRooms;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPrimaryDoctor() {
+        return primaryDoctor;
+    }
+
+    public void setPrimaryDoctor(String primaryDoctor) {
+        this.primaryDoctor = primaryDoctor;
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getOfficePhone() {
+        return officePhone;
+    }
+
+    public void setOfficePhone(String officePhone) {
+        this.officePhone = officePhone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getFormattedAddress() {
+        return formattedAddress;
+    }
+
+    public void setFormattedAddress(String formattedAddress) {
+        this.formattedAddress = formattedAddress;
+    }
+
+    public String getBillingName() {
+        return billingName;
+    }
+
+    public void setBillingName(String billingName) {
+        this.billingName = billingName;
+    }
+
+    public String getBillingBranch() {
+        return billingBranch;
+    }
+
+    public void setBillingBranch(String billingBranch) {
+        this.billingBranch = billingBranch;
+    }
+
+    public String getBillingTaxID() {
+        return billingTaxID;
+    }
+
+    public void setBillingTaxID(String billingTaxID) {
+        this.billingTaxID = billingTaxID;
+    }
+
+    public Boolean getShowBranchOnline() {
+        return showBranchOnline;
+    }
+
+    public void setShowBranchOnline(Boolean showBranchOnline) {
+        this.showBranchOnline = showBranchOnline;
+    }
+
+    public Boolean getAllowOnlineSchedulingInBranch() {
+        return allowOnlineSchedulingInBranch;
+    }
+
+    public void setAllowOnlineSchedulingInBranch(Boolean allowOnlineSchedulingInBranch) {
+        this.allowOnlineSchedulingInBranch = allowOnlineSchedulingInBranch;
+    }
+
+    public Long getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(Long rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Room> getExamRooms() {
+        return examRooms;
+    }
+
+    public void setExamRooms(List<Room> examRooms) {
+        this.examRooms = examRooms;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Room getRoomList() {
+        return roomList;
+    }
+
+    public void setRoomList(Room roomList) {
+        this.roomList = roomList;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
