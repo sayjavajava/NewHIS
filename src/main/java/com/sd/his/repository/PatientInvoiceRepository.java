350743134c13765bcafa55b8df32790334268389
awais jilani <awais.jilani@sdots.local>
2018-10-05 12:22:23 +0500

    patient balance working in demographics

--- src/main/java/com/sd/his/repository/PatientInvoiceRepository.java
+++ src/main/java/com/sd/his/repository/PatientInvoiceRepository.java
@@ -4,6 +4,7 @@ import com.sd.his.model.Invoice;
 import com.sd.his.wrapper.response.InvoiceResponseWrapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,5 +18,7 @@ public interface PatientInvoiceRepository  extends JpaRepository<Invoice, Long> 
            " FROM Invoice invc ")
     List<InvoiceResponseWrapper> findAllInvoices();
 
-
+    @Query("SELECT NEW com.sd.his.wrapper.response.InvoiceResponseWrapper( ( SUM(invc.invoiceAmount)-SUM(invc.paidAmount) ), SUM(invc.invoiceAmount), SUM(invc.paidAmount), SUM(p.advanceBalance) ) "+
+            "FROM Invoice invc inner join invc.patient p WHERE p.id=:patientId ")
+    InvoiceResponseWrapper getPatientInvoicesBalance(@Param("patientId") Long patientId);
 }
