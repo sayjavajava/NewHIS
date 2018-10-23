e521783294428c1e59b66fca109b22320a39a584
Naeem Saeed <naeem@sdots.local>
2018-10-23 15:30:46 +0500

    CHart of account complete and account setup in progress

---
+++ src/main/java/com/sd/his/service/VitalSetupServices.java
@@ -0,0 +1,24 @@
+package com.sd.his.service;
+
+import com.sd.his.model.VitalSetup;
+import com.sd.his.repository.VitalSetupRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class VitalSetupServices {
+
+    @Autowired
+    VitalSetupRepository vitalSetupRepository;
+
+    public List<VitalSetup> getAll(){
+        return vitalSetupRepository.getAll();
+    }
+
+    public void saveConfiguration(VitalSetup vitalSetup){
+        vitalSetupRepository.save(vitalSetup);
+    }
+}
